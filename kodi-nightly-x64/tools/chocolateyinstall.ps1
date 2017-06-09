430dd61483bcc66521e3469abdcd1b2db3ab5839
Dan Gibbs <Xenuri@users.noreply.github.com>
2017-06-09 17:50:47 +0100

    Fixed bad formatting

--- kodi-nightly-x64/tools/chocolateyinstall.ps1
+++ kodi-nightly-x64/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 # Install package
 $packageArgs = @{
 	packageName 	=	'kodi-nightly-x64'
-	fileType		=	'EXE'
-    url  			=	'http://mirrors.kodi.tv/nightlies/win64/master/KodiSetup-20170607-79519e05f0-master-x64.exe'
+	fileType	=	'EXE'
+        url  		=	'http://mirrors.kodi.tv/nightlies/win64/master/KodiSetup-20170607-79519e05f0-master-x64.exe'
 	checksum        =   'd9d8b123086e2919ba401967fae2a5241d4367cd632588f4730210b0f484749a'
 	checksumType    =   'sha256'
-	silentArgs		=	"/S"
+	silentArgs	=	"/S"
 	validExitCodes	=	@(0)
 }
 Install-ChocolateyPackage @packageArgs
