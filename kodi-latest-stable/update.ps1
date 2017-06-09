08fc97d89c3d545f8f2156c043074d0d467d6b3c
Dan Gibbs <Xenuri@users.noreply.github.com>
2017-06-09 19:55:50 +0100

    Fixed bad formatting

--- kodi-latest-stable/update.ps1
+++ kodi-latest-stable/update.ps1
@@ -13,9 +13,9 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
      $download_page = Invoke-WebRequest -Uri $releases
-	 $url = $download_page.links | ? href -match '\.exe$' | % href | select -First 1
-	 $url = $releases + $url
-	 $version = $url -split '-' | select -First 1 -Skip 1
+     $url = $download_page.links | ? href -match '\.exe$' | % href | select -First 1
+     $url = $releases + $url
+     $version = $url -split '-' | select -First 1 -Skip 1
      return @{ Version = $version; URL32 = $url }
 }
 
