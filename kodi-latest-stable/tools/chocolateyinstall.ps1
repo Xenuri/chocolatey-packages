98e6a3bc8c5785f5ed04aab2ca92d254361320a3
Dan Gibbs <Xenuri@users.noreply.github.com>
2017-06-09 19:55:10 +0100

    Fixed bad formatting

--- kodi-latest-stable/tools/chocolateyinstall.ps1
+++ kodi-latest-stable/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
 	packageName 	=	'kodi-latest-stable'
 	fileType	=	'EXE'
         url  		=	'http://mirrors.kodi.tv/releases/win32/kodi-17.3-Krypton.exe'
-	Checksum        =   '871a8c431515e68d62090022c55fe9cb2f95e65804a7ccff51929b1a7e6be890'
-	ChecksumType    =   'sha256'
+	Checksum        =       '871a8c431515e68d62090022c55fe9cb2f95e65804a7ccff51929b1a7e6be890'
+	ChecksumType    =       'sha256'
 	silentArgs	=	"/S"
 	validExitCodes	=	@(0)
 }
