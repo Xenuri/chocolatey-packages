add249c8422ba7f1dba4cc80f62187de446fc1a3
Chocolatey <chocolatey@realdimensions.net>
2017-06-10 06:03:23 +0000

    AU: 2 updated - kodi-nightly-x64 kodi-nightly-x86 [skip ci] https://gist.github.com/939bbfe393295a0ac1b89001d82a8868/63f1ffbec4b064153231f3b7da8e13484f997926

--- kodi-nightly-x64/tools/chocolateyinstall.ps1
+++ kodi-nightly-x64/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@
 $packageArgs = @{
 	packageName 	=	'kodi-nightly-x64'
 	fileType	=	'EXE'
-        url  		=	'http://mirrors.kodi.tv/nightlies/win64/master/KodiSetup-20170607-79519e05f0-master-x64.exe'
-	checksum        =       'd9d8b123086e2919ba401967fae2a5241d4367cd632588f4730210b0f484749a'
+        url  		=	'http://mirrors.kodi.tv/nightlies/win64/master/KodiSetup-20170609-5cc87efd62-master-x64.exe'
+	checksum        =       '1b405013cf23ab8361f8a0f2dad1117e3a5787f5d43b4cf3548f3304f60e6c70'
 	checksumType    =       'sha256'
 	silentArgs	=	"/S"
 	validExitCodes	=	@(0)
--- kodi-nightly-x86/Tools/chocolateyinstall.ps1
+++ kodi-nightly-x86/Tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@
 $packageArgs = @{
 	packageName 	=	'kodi-nightly-x86'
 	fileType	=	'EXE'
-        url  		=	'http://mirrors.kodi.tv/nightlies/win32/master/KodiSetup-20170607-79519e0-master-x86.exe'
-	checksum        =       '946121ab7abcaa95b82e4f0132a9c8f53118b0cacb320778d269883fa9615c76'
+        url  		=	'http://mirrors.kodi.tv/nightlies/win32/master/KodiSetup-20170609-5cc87efd62-master-x86.exe'
+	checksum        =       'be5e44a8fe12933c9c79e7f8b910ec26eb728daf8c3d82aa214b450e8fccd89e'
 	checksumType    =       'sha256'
 	silentArgs	=	"/S"
 	validExitCodes	=	@(0)
