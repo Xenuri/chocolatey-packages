b661775df4cfe54ee2b451d843c050027e47af81
Chocolatey <chocolatey@realdimensions.net>
2017-06-12 06:05:50 +0000

    AU: 1 updated - kodi-nightly-x86 [skip ci] https://gist.github.com/939bbfe393295a0ac1b89001d82a8868/03e7a25c238ab2cf4cf1f308543d9c60649aab8c

--- kodi-nightly-x86/Tools/chocolateyinstall.ps1
+++ kodi-nightly-x86/Tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@
 $packageArgs = @{
 	packageName 	=	'kodi-nightly-x86'
 	fileType	=	'EXE'
-        url  		=	'http://mirrors.kodi.tv/nightlies/win32/master/KodiSetup-20170610-a0a04d6011-master-x86.exe'
-	checksum        =       '5e15ab181518397b526732ac30b93a5315180784d245a181102eaf89c0158b39'
+        url  		=	'http://mirrors.kodi.tv/nightlies/win32/master/KodiSetup-20170612-540a0c7e9d-master-x86.exe'
+	checksum        =       'b85fdd84fed2163a7a67c53f988fbc5b2c478619ac2957d5d2937de9b78b47ca'
 	checksumType    =       'sha256'
 	silentArgs	=	"/S"
 	validExitCodes	=	@(0)
